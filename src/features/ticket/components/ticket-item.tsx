e7a77c4b29e06c792e945405856048125af6a4b2
Underprotection <mertcandemirkaya@outlook.com>
2025-08-09 01:17:53 +0300

    06: Server Actions Redirect

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -1,10 +1,11 @@
 import clsx from "clsx";
-import { LucideArrowUpRightFromSquare } from "lucide-react";
+import { LucideArrowUpRightFromSquare, LucideTrash } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Ticket } from "@/generated/prisma";
 import { ticketPath } from "@/paths";
+import { deleteTicket } from "../actions/delete-ticket";
 import { TICKET_ICONS } from "../constants";
 
 type TicketItemProps = {
@@ -13,14 +14,20 @@ type TicketItemProps = {
 };
 
 export const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
-  console.log("Where am I displayed? (TicketItem)");
-
   const detailButton = (
     <Button variant="outline" size="icon" asChild>
       <Link href={ticketPath(ticket.id)}>
         <LucideArrowUpRightFromSquare className="h-4 w-4" />
       </Link>
     </Button>
+  );
+
+  const deleteButton = (
+    <form action={deleteTicket.bind(null, ticket.id)}>
+      <Button variant="outline" size="icon" type="submit">
+        <LucideTrash className="size-4" />
+      </Button>
+    </form>
   );
 
   return (
@@ -47,10 +54,9 @@ export const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
           </span>
         </CardContent>
       </Card>
-
-      {isDetail ? null : (
-        <div className="flex flex-col gap-y-1">{detailButton}</div>
-      )}
+      <div className="flex flex-col gap-y-1">
+        {isDetail ? deleteButton : detailButton}
+      </div>
     </div>
   );
 };
