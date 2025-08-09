389005707a13a83a14af79c8e2cf95dd42f31a65
Underprotection <mertcandemirkaya@outlook.com>
2025-08-09 11:03:22 +0300

    07: Caching

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -16,7 +16,7 @@ type TicketItemProps = {
 export const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
   const detailButton = (
     <Button variant="outline" size="icon" asChild>
-      <Link href={ticketPath(ticket.id)}>
+      <Link prefetch href={ticketPath(ticket.id)}>
         <LucideArrowUpRightFromSquare className="h-4 w-4" />
       </Link>
     </Button>
